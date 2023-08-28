aac773f97d8dd9d4d23bbc9f5c935e1f4cc25263
artF412 <Chalantorn.sup@gmail.com>
2023-08-28 18:23:34 +0700

    Creating and Manipulating Objects

---
+++ create_and_anim/create_and_anim.py
@@ -0,0 +1,40 @@
+import maya.cmds as cmds
+import random
+
+
+# fix random
+random.seed(1234)
+
+# create polyCube width = 1 , height = 1 , depth = 1, name=set the name
+create_cube = cmds.polyCube(w=1,h=1,d=1, name='cube#')
+
+# get value transform node
+transformName = create_cube[0]
+
+# create group
+groupName = cmds.group(empty=True, name=transformName+'_grp#')
+
+# Create cube by number range
+for i in range(50):
+    # Duplicate objects from transformName
+    createCube = cmds.instance(transformName, name=transformName+'_#')
+    
+    # Set child and parent
+    cmds.parent(createCube, groupName)
+    
+    x = random.uniform(-10,10)
+    y = random.uniform(0,20)
+    z = random.uniform(-10,10)
+    cmds.move(x, y, z, createCube)
+    
+    xRot = random.uniform(0, 360)
+    yRot = random.uniform(0, 360)
+    zRot = random.uniform(0, 360)
+    cmds.rotate(xRot, yRot, zRot, createCube)
+    
+    scalingXYZ = random.uniform(0.3, 1.5)
+    cmds.scale(scalingXYZ, scalingXYZ, scalingXYZ, createCube)
+    
+cmds.xform(groupName, centerPivots=True)
+
+cmds.hide(transformName)
