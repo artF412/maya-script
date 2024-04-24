18902e1dd302cede3f3f3cfb2568690ecfdb1f21
ArtF412 <Chalantorn.sup@gmail.com>
2024-04-24 20:32:12 +0700

    Update rename_objects.py
    Write functions to sorting mesh and group for rename items. Next step write function to change name in the outliner

--- rename_objects/rename_objects.py
+++ rename_objects/rename_objects.py
@@ -1,6 +1,30 @@
 import maya.cmds as cmds
 
-def rename_objects():
-    pass
+def list_objects():
 
-rename_objects()
+    # List transform objects
+    transformObjs = cmds.ls(transforms=True)
+
+    # Take out camera viewport from transform objects
+    items_names = [obj for obj in transformObjs if obj not in ['persp', 'top', 'front', 'side']]
+
+    # Create list of objects mesh and group
+    mesh = []
+    grp = []
+
+    # Check if object is mesh or group
+    for items_name in items_names:
+        shapes = cmds.listRelatives(items_name, shapes=True)
+        if shapes:
+            mesh.append(items_name)
+        else:
+            grp.append(items_name)
+
+    # Return list of group and mesh
+    return grp, mesh
+
+# Testing
+items = list_objects()
+grp, mesh = items
+print("Groups :", grp)
+print("\nMeshes :", mesh)
